26e72d0886a00aeced199a62786a290dcb1eae7a
BugrinaA <tsaaeel@gmail.com>
2023-12-21 02:39:24 +0500

    Создание пользовательского элемента управления Списка отделов. Бугрина

---
+++ Elements/AddDepartaments.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupermarketBugrina.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDepartaments.xaml
+    /// </summary>
+    public partial class AddDepartaments : UserControl
+    {
+        public AddDepartaments()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
